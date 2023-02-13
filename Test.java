bbf28d23ded8e855b23465602cc10d802635862d
prabal kuinkel <prabalkuinkel10@gmail.com>
2023-02-13 19:48:51 +0700

    1st java assignment

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[] args)
+    {
+        Scanner input =new Scanner (System.in);
+        System.out.println("My name is prabal kuinkel");
+        System.out.println("Enter two numbers: ");
+        int a=input.nextInt();
+        System.out.println(a);
+        input.close();
+        
+
+    }
+}
