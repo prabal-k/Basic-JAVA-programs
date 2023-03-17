ee8e5e50807b4607b2018d0320fef56781dabe4f
prabal kuinkel <prabalkuinkel10@gmail.com>
2023-03-17 16:15:04 +0700

    Anagram updated

--- Anagram_word/src/Calculation.java
+++ Anagram_word/src/Calculation.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class Calculation {
     public static Scanner scan = new Scanner(System.in);
     static int flag = 0;
@@ -14,19 +14,21 @@ public class Calculation {
             int length2 = arr2.length();
             char ch1[] = arr.toCharArray();
             char ch2[] = arr2.toCharArray();
-            if (length1 == length2) {
-                for (int i = 0; i < length1; i++)
-                {
-                    for (int j = 0; j < length2; j++)
-                    {
-                        if (ch1[i] == ch2[j])
-                        {
-                            count++;
-                            //break;
+            Arrays.sort(ch1);
+            Arrays.sort(ch2);
+            //String st = new String(ch1);
+            //String st2 = new String(ch2);
+            //System.out.println(ch1+" "+ch2);
+
+                if (length1 == length2) {
+                    for (int i = 0; i < length1; i++)
+                         {
+                            if (ch1[i] == ch2[i]) {
+                                count++;
+                                //break;
+                            }
                         }
                     }
-                }
-            }
             else
                 return 0;
             if (count == length1)
